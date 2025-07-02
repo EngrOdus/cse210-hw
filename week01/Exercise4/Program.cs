d85f4fe9d5d02456790b12a6ca7fe38b241b5e5a
EngrOdus <henry.odumamwen@outlook.com>
2025-07-02 12:59:09 +0100

    Created The List<int> number = new List<int>()

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Hello World! This is the Exercise4 Working with a List Project.");
+
+        List<int> number = new List<int>();
+
+        int yourNumber = 1;
+        while (yourNumber != 0)
+        {
+            Console.Write("Enter a list of numbers and type 0 when finished. ");
+            yourNumber = int.Parse(Console.ReadLine());
+
+            number.Add(yourNumber);
+        }
     }
 }
