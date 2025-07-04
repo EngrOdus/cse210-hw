b851a127f002476228d964be075698ab227398a6
EngrOdus <henry.odumamwen@outlook.com>
2025-07-04 17:34:31 +0100

    Created parts of the Resume.cs files.

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,1 +1,9 @@
-public class Resume;
+public class Resume
+{
+    public string _personalName;
+    public List<Job> _job = new List<Job>();
+}
+foreach (Job j in _job)
+{
+    Console.WriteLine($"{}{}")
+}
