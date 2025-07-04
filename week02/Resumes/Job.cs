c9f2fab7f2f1b2a5b9232fa4d5e0049df49583ca
EngrOdus <henry.odumamwen@outlook.com>
2025-07-04 17:22:59 +0100

    Created Job.cs files.

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,11 @@
+public class Job
+{
+    public string _companyName;
+    public string _jobTitle;
+    public double _startYear;
+    public double _endYear;
+    public Job()
+    {
+        
+    }
+}
