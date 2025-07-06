65f5eac7aea6a33318817fc0309cd32b75eda19f
EngrOdus <henry.odumamwen@outlook.com>
2025-07-07 00:51:48 +0100

    Completed the Resume, Job, Program files.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Take the first job information
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        Console.Write("What is the name of the first company you worked in? ");
+        job1._companyName = Console.ReadLine();
+        Console.Write("What was your role in this company? ");
+        job1._jobTitle = Console.ReadLine();
+        Console.Write("What was the year you started working here? ");
+        job1._startYear = double.Parse(Console.ReadLine());
+        Console.Write("What was the year you stopped working here? ");
+        job1._endYear = double.Parse(Console.ReadLine());
+
+        Console.Write("What is the name of the second company you worked in? ");
+        job2._companyName = Console.ReadLine();
+        Console.Write("What was your role in this company? ");
+        job2._jobTitle = Console.ReadLine();
+        Console.Write("What was the year you started working here? ");
+        job2._startYear = double.Parse(Console.ReadLine());
+        Console.Write("What was the year you stopped working here? ");
+        job2._endYear = double.Parse(Console.ReadLine());
+
+        Resume resume = new Resume(); //Created th e Resume class object as resume.
+        Console.Write("Please enter your name! ");
+        resume._personalName = Console.ReadLine();
+        resume._job.Add(job1);
+        resume._job.Add(job2);
+
+        resume.DisplayResume(); //Called the DisplayResume() metthod.
     }
 }
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -2,8 +2,21 @@ public class Resume
 {
     public string _personalName;
     public List<Job> _job = new List<Job>();
-}
-foreach (Job j in _job)
-{
-    Console.WriteLine($"{}{}")
+
+    public Resume() //created the Resume class constructor.
+    {
+
+    }
+
+    public void DisplayResume()
+    {
+        int jobCount = 0;
+        Console.WriteLine($"{_personalName}");
+        Console.WriteLine("Jobs Experience:");
+        foreach (Job j in _job)
+        {
+            jobCount += 1;
+            Console.WriteLine($"({jobCount}) Company: {j._companyName}, Job Title: {j._jobTitle}, Start Year: {j._startYear} - End Year: {j._endYear} for duration of {j._endYear - j._startYear}yrs.");
+        }
+    }
 }
