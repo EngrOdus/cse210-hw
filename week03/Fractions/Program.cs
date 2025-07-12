50e05f67b536dfd16bd151fcbe6f4ea587a29e17
EngrOdus <henry.odumamwen@outlook.com>
2025-07-12 21:20:10 +0100

    Created three fraction1 ,2 & 3 objects for all three constructors respectivrly.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+
+        Fraction fraction1 = new Fraction();
+
+        Fraction fraction2 = new Fraction(6);
+
+        Fraction fraction3 = new Fraction(6, 7);
     }
 }
