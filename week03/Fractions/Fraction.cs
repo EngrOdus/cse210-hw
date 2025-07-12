1cc1c96fc886bbc71d289a54b30285da81835059
EngrOdus <henry.odumamwen@outlook.com>
2025-07-13 00:10:19 +0100

    Added comments for readability.

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -5,20 +5,20 @@ public class Fraction
 
     public Fraction()
     {
-        _top = 1;
-        _bottom = 1;
+        _top = 1; // initializes the class attribute "_top" of the fraction1 object to 1.
+        _bottom = 1; // initializes the class attribute "_bottom" of the fraction1 object to 1.
     }
 
     public Fraction(int wholeNumber)
     {
-        _top = wholeNumber;
-        _bottom = 1;
+        _top = wholeNumber; // Store the wholeNumber value from fraction2 object into the class attribute "_top" of the fraction2 object
+        _bottom = 1; // initializes the class attribute "_bottom" of the fraction2 object to 1.
     }
 
     public Fraction(int top, int bottom)
     {
-        _top = top;
-        _bottom = bottom;
+        _top = top; // Store the top value from the fraction3 & fraction4 objects into the class attribute "_top" of the fraction3 & fraction4 objects.
+        _bottom = bottom; // Store the bottom value from the fraction3 & fraction4 objects into the class attribute "_bottom" of the fraction3 and fraction4 objects.
     }
 
     public int GetTop()
@@ -42,16 +42,16 @@ public class Fraction
 
     public string GetFractionString(string top, string bottom)
     {
-        top = GetTop().ToString();
-        bottom = GetBottom().ToString();
-        return top + "/" + bottom;
+        top = GetTop().ToString();// Convert to string
+        bottom = GetBottom().ToString();//Convert to string.
+        return top + "/" + bottom;//Returned the string representation.
     }
 
     public Decimal GetDecimalValues(int topValue, int bottomValue)
     {
         topValue = _top;
         bottomValue = _bottom;
-        Decimal result = Decimal.Divide(topValue, bottomValue);
-        return result;
+        Decimal result = Decimal.Divide(topValue, bottomValue); // converted from int to decimal points value using C#.
+        return result; //Return the decimal value.
     }
 }
