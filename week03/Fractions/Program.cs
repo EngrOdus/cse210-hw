8fed4baa2d6b2ebb11901a6e067b88d65769b00b
EngrOdus <henry.odumamwen@outlook.com>
2025-07-13 00:15:06 +0100

    Added comments to the print statemants.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -8,19 +8,19 @@ class Program
 
 
         Fraction fraction1 = new Fraction();
-        Console.WriteLine(fraction1.GetFractionString("1", "1"));
-        Console.WriteLine(fraction1.GetDecimalValues(1, 1));
+        Console.WriteLine(fraction1.GetFractionString("1", "1")); //Print the string representation.
+        Console.WriteLine(fraction1.GetDecimalValues(1, 1)); //Print the decimal value.
 
         Fraction fraction2 = new Fraction(5);
-        Console.WriteLine(fraction2.GetFractionString("1", "1"));
-        Console.WriteLine(fraction2.GetDecimalValues(1, 1));
+        Console.WriteLine(fraction2.GetFractionString("1", "1")); //Print the string representation.
+        Console.WriteLine(fraction2.GetDecimalValues(1, 1)); //Print the decimal value.
 
         Fraction fraction3 = new Fraction(3, 4);
-        Console.WriteLine(fraction3.GetFractionString("1", "1"));
-        Console.WriteLine(fraction3.GetDecimalValues(1, 1));
+        Console.WriteLine(fraction3.GetFractionString("1", "1")); //Print the string representation.
+        Console.WriteLine(fraction3.GetDecimalValues(1, 1)); //Print the decimal value.
 
         Fraction fraction4 = new Fraction(1, 3);
-        Console.WriteLine(fraction4.GetFractionString("1", "1"));
-        Console.WriteLine($"{fraction4.GetDecimalValues(1, 1)}");
+        Console.WriteLine(fraction4.GetFractionString("1", "1")); //Print the string representation.
+        Console.WriteLine($"{fraction4.GetDecimalValues(1, 1)}"); //Print the decimal value.
     }
 }
