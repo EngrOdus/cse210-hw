6cdc9352d7212e6bcb18d67db2d718f95087e53b
EngrOdus <henry.odumamwen@outlook.com>
2025-07-14 23:11:03 +0100

    Developed the Referece class with its Getter, Setter and method code.

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,10 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Reference
 {
     private string _scriptureReference;
-    private List<string> _references = ["Alma 5:2", "John 1:1", "John 15:4", "1Nephi 2:2"];
+    //private List<string> _reference = new List<string>();
+
+    private string[] _reference = { "Alma 2:2", "John 15:4", "2Nephi 2:2", "1Cor 15:29", "Moses 6: 65" };
+
 
     public Reference()
     {
-        
+
+    }
+
+    public void SetReference()
+    {
+        _scriptureReference = GetRandomReference();
+    }
+
+    public string GetReference()
+    {
+        SetReference();
+        return _scriptureReference;
+    }
+
+    public string GetRandomReference()
+    {
+
+        Random randomReference = new Random();
+
+        string rand = _reference[randomReference.Next(0, _reference.Length)];
+
+        return rand;
     }
 }
