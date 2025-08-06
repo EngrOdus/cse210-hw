5d422a633dc72d406a2263a69c1f4bf87143f5fe
EngrOdus <henry.odumamwen@outlook.com>
2025-08-06 14:44:00 +0100

    Added Employee folder for employee applications

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        string[] animals = { "cow", "goat", "pig", "birds", "dog", "antelope" };
+        List<string> animalList = new List<string>();
+        animalList.AddRange(animals);
+        Console.Clear();
+        foreach (string ani in animalList)
+        {
+            Console.WriteLine(ani);
+        }
+
+        for (int i = 0; i < animalList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}: {animalList[i]}");
+
+            List<string> names = new List<string>() { "<name>", "john", "paul", "tr", "df", "56" };
+            Console.WriteLine(names.Count);
+            Console.WriteLine(names.Count());
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.ToUpper());
+            }
+
+            names.ForEach(i => Console.WriteLine(i));
+        }
     }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,36 @@
 using System;
-
+using System.Threading;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+        Console.Write("Enter countdown time in seconds: ");
+        if (int.TryParse(Console.ReadLine(), out int totalSeconds) && totalSeconds > 0)
+        {
+            Console.WriteLine("Breath in...\n");
+
+            for (int i = totalSeconds; i >= 0; i--)
+            {
+                Console.Clear();
+                Console.WriteLine($"Time left: {i} second{(i != 1 ? "s" : "")}");
+                Thread.Sleep(3000); // Pause for 3 second
+            }
+
+            Console.Clear();
+            Console.WriteLine("⏰ Time's up! \n Breath Out!");
+            Thread.Sleep(3000); // Pause for 3 second
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a positive number.");
+        }
+
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
+    
+
+
     }
 }
